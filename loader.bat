a0f83c46a70d75a095d3f34b936adaf5b54bc406
Up0k8 <134734746+Upok18@users.noreply.github.com>
2026-03-15 02:07:53 +0600

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -2,7 +2,7 @@
 title Up0ktilizer Loader
 
 set url=https://raw.githubusercontent.com/Upok18/Up0ktilizer/refs/heads/main/tool.bat
-set tempFile=%temp%\up0k_tool.bat
+set tempFile=%temp%\Up0ktilizer_tool.bat
 
 echo Downloading Up0ktilizer...
 curl -L -o "%tempFile%" "%url%"
