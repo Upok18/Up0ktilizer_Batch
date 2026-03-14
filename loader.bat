45b23a119630f25705cb53bacbab7e95ad67e2d7
Up0k8 <134734746+Upok18@users.noreply.github.com>
2026-03-15 03:34:54 +0600

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -1,16 +1,33 @@
 @echo off
-title Up0ktilizer Loader
 
-set tempFile=%temp%\up0k_tool.bat
-set url=https://raw.githubusercontent.com/Upok18/Up0ktilizer/refs/heads/main/tool.bat?%random%
+set tempTool=%temp%\up0k_tool.bat
+set tempVer=%temp%\up0k_version.txt
 
-del "%tempFile%" >nul 2>&1
+set toolURL=https://raw.githubusercontent.com/Upok18/Up0ktilizer/refs/heads/main/tool.bat?%random%
+set verURL=https://raw.githubusercontent.com/Upok18/Up0ktilizer/refs/heads/main/version.txt?%random%
 
-echo Downloading latest version...
-curl -L "%url%" -o "%tempFile%"
+echo Checking latest version...
 
-echo Starting tool...
-call "%tempFile%"
+curl -sL "%verURL%" -o "%tempVer%"
+set /p remoteVer=<"%tempVer%"
 
-del "%tempFile%"
+if exist "%tempTool%" (
+    findstr "%remoteVer%" "%tempTool%" >nul
+    if %errorlevel%==0 (
+        echo Tool already up to date.
+        goto runTool
+    )
+)
+
+echo Updating tool...
+curl -sL "%toolURL%" -o "%tempTool%"
+
+:runTool
+call "%tempTool%"
+
+echo Cleaning up...
+
+del "%tempTool%" >nul 2>&1
+del "%tempVer%" >nul 2>&1
+
 exit
